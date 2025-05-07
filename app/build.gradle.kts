f5f4e67c20f06d55abf4f6a9703b9151419a6ca1
wilbertb32 <wilbertbernardi2004@gmail.com>
2025-05-07 08:42:03 +0700

    Write unit tests to test coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,9 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    testImplementation(kotlin("test"))
 }
--- app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -16,7 +16,17 @@
 package com.example.racetracker
 
 import com.example.racetracker.ui.RaceParticipant
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class RaceParticipantTest {
     private val raceParticipant = RaceParticipant(
         name = "Test",
@@ -25,4 +35,56 @@ class RaceParticipantTest {
         initialProgress = 0,
         progressIncrement = 1
     )
+
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+        runCurrent()
+        Assert.assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        launch { raceParticipant.run() }
+
+        advanceTimeBy(raceParticipant.maxProgress * raceParticipant.progressDelayMillis)
+        runCurrent()
+        Assert.assertEquals(100, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RacePaused_ProgressUpdated() = runTest {
+        val expectedProgress = 5
+        val racerJob = launch { raceParticipant.run() }
+        advanceTimeBy(expectedProgress * raceParticipant.progressDelayMillis)
+        runCurrent()
+        racerJob.cancelAndJoin()
+        Assert.assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RacePausedAndResumed_ProgressUpdated() = runTest {
+        val expectedProgress = 5
+
+        repeat(2) {
+            val racerJob = launch { raceParticipant.run() }
+            advanceTimeBy(expectedProgress * raceParticipant.progressDelayMillis)
+            runCurrent()
+            racerJob.cancelAndJoin()
+        }
+
+        Assert.assertEquals(expectedProgress * 2, raceParticipant.currentProgress)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun raceParticipant_ProgressIncrementZero_ExceptionThrown() = runTest {
+        RaceParticipant(name = "Progress Test", progressIncrement = 0)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun raceParticipant_MaxProgressZero_ExceptionThrown() {
+        RaceParticipant(name = "Progress Test", maxProgress = 0)
+    }
 }
